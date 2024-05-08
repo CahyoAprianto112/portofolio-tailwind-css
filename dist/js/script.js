f032e535e2073a3e3c16b774a4331bf005a9a74a
CahyoAprianto9 <nurcahyo81821@gmail.com>
2024-05-08 09:00:16 +0700

    portofolio tailwind css

---
+++ dist/js/script.js
@@ -0,0 +1,24 @@
+// Navbar Fixed
+// ketika window scrool jalankan function ini
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    // cek scroll position
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+
+
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
